697face142caae10417a5666bc33ced22536c919
LasataM-123 <lasatamaharjan2006@gmail.com>
2025-05-25 15:05:24 +0545

    hjuhu

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+const upload=multer({dest:'./public/'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", upload.single('upfile'),function (req, res) {
+  const file=req.file;
+  res.json({name:file.originalname, type:file.mimetype, size:file.size});
+})
 
 
 const port = process.env.PORT || 3000;
